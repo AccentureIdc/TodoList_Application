33bfb54625d269e0693a9872ec54e8d9275e6119
Accentureidc <Accentureidcdevops@gmail.com>
2016-02-27 15:47:56 +0530

    chrome removed from test-browser

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -87,8 +87,7 @@ module.exports = function(config) {
     // - PhantomJS
     // - IE (only Windows)
     browsers: [
-      "PhantomJS",
-      "Chrome"
+      "PhantomJS"
     ],
 
     // Which plugins to enable
